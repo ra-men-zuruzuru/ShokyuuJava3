e33e08809cfd3f44aa5524f3f3ce2ef8407b8776
igada <iga.dai0226@gmail.com>
2024-05-13 12:03:17 +0900

    練習問題2-1の追加

--- 初級Java2.5/src/Start.java
+++ 初級Java2.5/src/Start.java
@@ -2,8 +2,12 @@
 public class Start {
 
 	public static void main(String[] args) {
+		int x=2;
+		int y=3;
+		System.out.println(x*y);
 		for(int i=0;i<2;i++) {
 		MyFrame frame3=new RectFrame();
+		frame3.setLocation(50,300);
 		}
 		// TODO 自動生成されたメソッド・スタブ
 	}
