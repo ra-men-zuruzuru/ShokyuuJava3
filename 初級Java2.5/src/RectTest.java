2abf9a148ae63b8785ec585c4afd5afbb45fdb7e
igada <iga.dai0226@gmail.com>
2024-05-14 12:10:43 +0900

    3.3のコミット

--- 初級Java2.5/src/RectTest.java
+++ 初級Java2.5/src/RectTest.java
@@ -1,9 +1,20 @@
 
 public class RectTest extends MyFrame {
 	public void run() {
+		//黄色
+		setColor(255,255,0);
 		fillRect(30,40,50,100);
+		//紫
+		setColor(255,0,255);
 		fillRect(130,40,50,100);
-		fillRect(30,200,150,10);
-		fillRect(90,160,30,30);
+		//水色
+		setColor(0,255,255);
+		fillRect(230,40,50,100);
+		//黒
+		setColor(0,0,0);
+		fillRect(330,40,50,100);
+		//灰色
+		setColor(128,128,128);
+		fillRect(30,200,150,100);
 	}
 }
