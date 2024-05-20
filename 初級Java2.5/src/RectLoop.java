624761efcbf50e154cd1df661372b8831e865696
igada <iga.dai0226@gmail.com>
2024-05-20 12:16:22 +0900

    practice4-2

--- 初級Java2.5/src/RectLoop.java
+++ 初級Java2.5/src/RectLoop.java
@@ -2,10 +2,10 @@ public class RectLoop extends MyFrame {
 	public void run() {
 		setColor(0,128,0);
 		int x=30;
-		int y=0;
+		int y=10;
 		for(int i=0;i<10;i++) {
 			System.out.println(x);
-			fillRect(x,y,10,100);
+			fillRect(x,50,10,y);
 			x+=20;
 			y+=30;
 		}
