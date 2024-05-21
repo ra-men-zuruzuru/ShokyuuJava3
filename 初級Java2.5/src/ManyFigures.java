03f40e57183170d34d164ca5ba428a88af696e51
igada <iga.dai0226@gmail.com>
2024-05-21 11:51:08 +0900

    おまけ問題その2

--- 初級Java2.5/src/ManyFigures.java
+++ 初級Java2.5/src/ManyFigures.java
@@ -1,19 +1,14 @@
 
 public class ManyFigures extends MyFrame {
 	public void run() {
-		//追加課題その２
-		setColor(0,128,0);
-		fillRect(100,100,200,200);
-		int y=250;
-		int x=100;
-		int xx=110;
-		setColor(256,256,256);
-		for(int i=0;i<10;i++) {
-			fillRect(x,y,10,30);
-			fillRect(xx,100,10,200);
-			x+=20;
-			y-=10;
-			xx+=20;
+		
+			setColor(0,128,0);//追加課題その１
+			int x=40;
+			int y=30;
+			for(int i=0;i<15;i++) {
+				fillRect(x,x,y,y);
+				x+=30+i*10;
+				y+=10;
 		}
 	}
 
