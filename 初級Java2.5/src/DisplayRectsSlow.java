f89c56e223ae29aeafa16018ba053345f7b8025a
igada <iga.dai0226@gmail.com>
2024-05-21 11:58:41 +0900

    check5-1

--- 初級Java2.5/src/DisplayRectsSlow.java
+++ 初級Java2.5/src/DisplayRectsSlow.java
@@ -2,10 +2,12 @@
 public class DisplayRectsSlow extends MyFrame{
 	public void run() {
 		int x=30;
+		int y=80;
 		setColor(0,128,0);
 		while(x<=170) {
-			fillRect(x,80,10,100);
+			fillRect(x,y,10,100);
 			x+=20;
+			y+=30;
 			sleep(1);
 		}
 	}
