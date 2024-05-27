a2140d04804988e59ca49741a9b3e74efb8f73e3
igada <iga.dai0226@gmail.com>
2024-05-27 10:47:08 +0900

    5.3のコミット

--- 初級Java2.5/src/Animation.java
+++ 初級Java2.5/src/Animation.java
@@ -1,15 +1,24 @@
 
 public class Animation extends MyFrame{
 	public void run() {
+		int y=100;
 		int x=30;
 		setColor(0,128,0);
+		while(true) {
+			
+		
 		while(x<=170) {
 			clear();
-			fillRect(x,80,10,100);
+			fillRect(x,y,10,100);
 			x+=5;
 			sleep(0.1);
 		}
-			
+		while(x>=30) {
+			clear();
+			fillRect(x,y,10,100);
+			x-=5;
+			sleep(0.1);
+		}	
+		}
 	}
-
 }
