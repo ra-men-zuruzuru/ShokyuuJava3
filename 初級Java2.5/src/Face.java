fb5e66dc3c574dee80e2e65232c74cf4cb7ac05a
igada <iga.dai0226@gmail.com>
2024-05-28 10:06:10 +0900

    6.2コミット

--- 初級Java2.5/src/Face.java
+++ 初級Java2.5/src/Face.java
@@ -1,10 +1,14 @@
 
 public class Face {
 	int x,y;
+	public Face(int x,int y) {
+		this.x=x;
+		this.y=y;
+	}
 	public void Draw(MyFrame frame) {
 		frame.fillOval(x,y,50,100);
 		frame.fillOval(x+100,y,50,100);
-		frame.fillOval(x,y+160,150,10);
+		frame.fillRect(x,y+160,150,10);
 	}
 
 }
