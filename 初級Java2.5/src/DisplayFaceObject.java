1d3862d1b07875e12f7265a03b2e0c6bcb4dcaa4
igada <iga.dai0226@gmail.com>
2024-05-28 12:06:16 +0900

    7.4コミット

--- 初級Java2.5/src/DisplayFaceObject.java
+++ 初級Java2.5/src/DisplayFaceObject.java
@@ -1,13 +1,14 @@
 
 public class DisplayFaceObject extends MyFrame {
 	public void run() {
-		Face[] faces = new Face[3];
+		Face[] faces = new Face[4];
 		faces[0] = new Face(50, 50, 10, 5);
 		faces[1] = new Face(200, 100, -10, -5);
 		faces[2] = new Face(0, 100, -10, 0);
+		faces[3]=new Face(200,100,-5,0);
 		for (int i = 0; i < 30; i++) {
 			clear();
-			for (int j = 0; j < 3; j++) {
+			for (int j = 0; j < 4; j++) {
 				faces[j].Draw(this);
 				faces[j].move();
 			}
