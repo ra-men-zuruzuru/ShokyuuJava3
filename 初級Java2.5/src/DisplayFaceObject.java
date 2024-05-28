59e952d940430119d317c6e31d068a3fc2308c52
igada <iga.dai0226@gmail.com>
2024-05-28 10:19:04 +0900

    6.3コミット

--- 初級Java2.5/src/DisplayFaceObject.java
+++ 初級Java2.5/src/DisplayFaceObject.java
@@ -6,6 +6,16 @@ public class DisplayFaceObject extends MyFrame{
 		
 		Face face2=new Face(200,100);
 		face2.Draw(this);
+		for(int i=0;i<30;i++) {
+			clear();
+			face1.Draw(this);
+			face2.Draw(this);
+			face1.x+=10;
+			face1.y+=5;
+			face2.x-=10;
+			face2.y-=5;
+			sleep(0.1);
+		}
 	}
 
 }
