e0c7a4472be18f9fb3cf009154b4f0a19222df9e
igada <iga.dai0226@gmail.com>
2024-06-11 09:50:33 +0900

    9.2コミット

--- 初級Java2.5/src/PlayerAnimation.java
+++ 初級Java2.5/src/PlayerAnimation.java
@@ -2,6 +2,7 @@
 public class PlayerAnimation extends MyFrame {
 	public void run() {
 		car player=new car(100,200,0,0);
+		addKeyListener(player);
 		while(true) {
 			clear();
 			player.Draw(this);
--- 初級Java2.5/src/car.java
+++ 初級Java2.5/src/car.java
@@ -1,5 +1,7 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class car extends Vehicle {
+public class car extends Vehicle implements KeyListener {
 	public car(int x,int y,int vx ,int vy) {
 		super(x,y,vx,vy);
 	}
@@ -8,6 +10,36 @@ public class car extends Vehicle {
 		frame.fillRect(x,y+20,80,20);
 		frame.fillOval(x+10,y+40,20,20);
 		frame.fillOval(x+50,y+40,20,20);
+	}
+	
+	//キーから文字が入力されたとき
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+	
+	//キーが押されたとき
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode()==KeyEvent.VK_LEFT) {
+			vx=-5;
+		}
+
+		if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
+			vx=5;
+		}
+		// TODO 自動生成されたメソッド・スタブ
+	}
+	
+	//キーが離されたとき
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if(e.getKeyCode()==KeyEvent.VK_LEFT ||
+				e.getKeyCode()==KeyEvent.VK_LEFT) {
+			vx=0;
+		}
+		// TODO 自動生成されたメソッド・スタブ
 	}
 
 }
